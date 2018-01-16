00cde408eff960dd3de3dea7fc33841bcadcef9d
Chris Riley <chris.riley@cprime.com>
2018-01-16 14:06:11 -0500

    Added protractor example and specs directory.

---
+++ webpackjs/src/specs/protractor.conf.js
@@ -0,0 +1,12 @@
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todo-spec.js'],
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+     args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+   }
+  }	
+}
